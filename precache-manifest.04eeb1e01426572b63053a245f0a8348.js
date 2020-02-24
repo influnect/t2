ea26eb082011d7c48ec3ba2635023aaeb34e0baf
Shivam Goraksha <s.goraksha@jacobs-university.de>
2020-02-24 17:22:23 +0100

    Updates

---
+++ precache-manifest.04eeb1e01426572b63053a245f0a8348.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183e7feb1d1e7fa690a9ed969df1d89a",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "6dcbac0a6c7caddcbdc2",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "241abb0a83e753575532",
+    "url": "/my-app/static/js/2.bcc72c2a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.bcc72c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dcbac0a6c7caddcbdc2",
+    "url": "/my-app/static/js/main.cd52ea7e.chunk.js"
+  },
+  {
+    "revision": "0c10fa8248c25a2f1677",
+    "url": "/my-app/static/js/runtime-main.dd12a99c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
